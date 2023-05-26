023e3a344633eb1d0fd58dbb236d03dee696f1b5
sathwik073 <106665429+sathwik073@users.noreply.github.com>
2023-05-26 15:17:43 +0530

    Initial commit

--- app/src/main/java/com/example/student/MainActivity.java
+++ app/src/main/java/com/example/student/MainActivity.java
@@ -22,5 +22,9 @@ public class MainActivity extends AppCompatActivity {
     public void admlgn(View view) {
         Intent i=new Intent(MainActivity.this,AdminLogin.class);
         startActivity(i);
+<<<<<<< HEAD
     }
+=======
+    }//hello
+>>>>>>> 5cd6a29 (Initial commit)
 }
